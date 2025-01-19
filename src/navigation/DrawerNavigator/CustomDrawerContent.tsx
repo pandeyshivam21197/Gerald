afe828a94b30d333f900b65529529f3ef55b47ee
Shivam Pandey <shivam@Shivams-MacBook-Pro.local>
2025-01-19 19:43:44 +0530

    Shivam: added styling to drawer nav button

--- src/navigation/DrawerNavigator/CustomDrawerContent.tsx
+++ src/navigation/DrawerNavigator/CustomDrawerContent.tsx
@@ -1,11 +1,11 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {
   DrawerContentComponentProps,
   DrawerContentScrollView,
   DrawerItem,
 } from '@react-navigation/drawer';
 import {NavigationScreenNames} from '../contants';
-import {StyleSheet, ViewStyle} from 'react-native';
+import {StyleSheet, Text, ViewStyle} from 'react-native';
 import {StyleProp} from 'react-native';
 import Animated from 'react-native-reanimated';
 
@@ -15,42 +15,62 @@ interface IProps extends DrawerContentComponentProps {
 
 export const CustomDrawerContent: FC<IProps> = props => {
   const {navigation, drawerCustomContentAnimatedStyle} = props;
+  const [focusedNavName, setFocusedNavName] =
+    useState<NavigationScreenNames | null>(NavigationScreenNames.Screen1);
 
   return (
     <Animated.View style={[drawerCustomContentAnimatedStyle, styles.container]}>
       <DrawerContentScrollView
         {...props}
         contentContainerStyle={styles.drawerContainer}>
+        <Text style={styles.heading}>Beka</Text>
         <DrawerItem
-          focused
+          focused={focusedNavName === NavigationScreenNames.Screen1}
           label={NavigationScreenNames.Screen1}
+          labelStyle={styles.lableStyle}
           onPress={() => {
             navigation.navigate(NavigationScreenNames.BottomTab, {
               screen: NavigationScreenNames.HomeStack,
               params: {screen: NavigationScreenNames.Screen1},
             });
+            setFocusedNavName(NavigationScreenNames.Screen1);
           }}
+          activeTintColor="#F9695C"
+          inactiveTintColor="white"
+          activeBackgroundColor="#412553"
           style={styles.drawerItem}
         />
         <DrawerItem
-          focused
+          focused={focusedNavName === NavigationScreenNames.Screen2}
           label={NavigationScreenNames.Screen2}
-          onPress={() =>
+          labelStyle={styles.lableStyle}
+          onPress={() => {
             navigation.navigate(NavigationScreenNames.BottomTab, {
               screen: NavigationScreenNames.HomeStack,
               params: {screen: NavigationScreenNames.Screen2},
-            })
-          }
+            });
+
+            setFocusedNavName(NavigationScreenNames.Screen2);
+          }}
+          activeTintColor="#F9695C"
+          inactiveTintColor="white"
+          activeBackgroundColor="#412553"
           style={styles.drawerItem}
         />
         <DrawerItem
-          focused
+          focused={focusedNavName === NavigationScreenNames.ContactStack}
           label={NavigationScreenNames.ContactScreen}
-          onPress={() =>
+          labelStyle={styles.lableStyle}
+          onPress={() => {
             navigation.navigate(NavigationScreenNames.BottomTab, {
               screen: NavigationScreenNames.ContactStack,
-            })
-          }
+            });
+
+            setFocusedNavName(NavigationScreenNames.ContactStack);
+          }}
+          activeTintColor="#F9695C"
+          inactiveTintColor="white"
+          activeBackgroundColor="#412553"
           style={styles.drawerItem}
         />
       </DrawerContentScrollView>
@@ -65,10 +85,23 @@ const styles = StyleSheet.create({
   },
   drawerItem: {
     width: '100%',
+    padding: 0,
+    borderRadius: 20,
   },
   drawerContainer: {
     flex: 1,
     flexDirection: 'column',
     gap: 20,
+    alignItems: 'center',
+    paddingTop: 100,
+  },
+  heading: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  lableStyle: {
+    fontSize: 20,
+    fontWeight: '600',
   },
 });
